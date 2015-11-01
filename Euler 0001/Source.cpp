c088e06f5f6fab64a987937bf868829a947506b8
Kelvin-Lee <orangejuiceboy@gmail.com>
2015-11-01 00:17:39 -0700

    Recommit: Euler 1, 6, and 7
    Euler 6 was in the wrong folder ("Euler 0003"). Removed it and placed it
    in its correct folder.
    
    Euler 1 and 7 are commented with problem descriptions, the solution,
    side-notes, and of course, comments to explain the program.
    
    Will continue this process for the rest of the Euler problems. Hopefully
    the current comment decoration process is sustainable.

--- Euler 0001/Source.cpp
+++ Euler 0001/Source.cpp
@@ -1,16 +1,52 @@
 #include <iostream>
+
 int main()
 {
 
+	/*     PROBLEM TITLE: " Multiples of 3 and 5 " 
+
+	   PROBLEM STATEMENT: " If we list all the natural 
+						    numbers below 10 that are multiples 
+							of 3 or 5, we get 3, 5, 6 and 9. The 
+							sum of these multiples is 23. Find the 
+							sum of all the multiples of 3 or 5 below 1000."
+
+	            SOLUTION: " 233168 " 
+
+	*/
+
+	/* Sums up all of the positive integers up to the upper_bound
+	   that are divisible by 3 or 5. If a number is divisible by both 
+	   3 and 5 (e.g. 15), then it is only added once.
+	*/
+
+	// Hard-coded upper-bound.
+	const int upper_bound = 1000;
+
 	int sum = 0;
-	for (int i = 0; i < 1000; i++) {
+
+	// For all of the number between 0 and the upper bound...
+	for (int i = 0; i < upper_bound; i++) {
+
+		// If the number is divisible by 3 or is divisible by 5...
 		if (i % 3 == 0 || i % 5 == 0)
+
 		{
-			printf("%d\n", i);
-			sum = i + sum;
+			// Add it to the running sum.
+			sum += i;
 		}
 	}
 	
-	printf("%d\n", sum);
-	return 0;
+	// Print out the sum.
+	std::cout << sum << std::endl;
+
+	return sum;
 }
+
+/* Notes:
+
+ - In the future, I'd like to code a more general solution that
+   has the user input choices for divisors, and also allows them
+   to choose an upper bound.
+
+*/
--- Euler 0007/Source.cpp
+++ Euler 0007/Source.cpp
@@ -1,42 +1,95 @@
 #include <iostream>
-#include <vector>
+#include <ctime>
 
 int main() {
 
-	std::cout << "Which prime do you want to find (1st ('1'), 2nd ('2'), etc.)?: ";
-	int bound = 0;
-	std::cin >> bound;
+	/*     PROBLEM TITLE: " 10001st Prime " 
 
-	std::vector<long> primes;
-	primes.push_back(2);
-	long l_last = primes.back();
+	   PROBLEM STATEMENT: " By listing the first six prime 
+							numbers: 2, 3, 5, 7, 11, and 13, 
+							we can see that the 6th prime is 13.
+							What is the 10 001st prime number?"
 
+	            SOLUTION: " 104743 " 
+	*/
+
+	#define BOUND 10001
+
+	// Hard-coded the length of the arry (must be known at
+	// compile time anyways).
+	unsigned long primes[BOUND];
+
+	// Inserted 2, the only even prime.
+	primes[0] = 2;
+
+	// Inserted 3, the first odd prime.
+	primes[1] = 3;
+
+	// Initialize the count to 2, the number
+	// of primes currently in our array.
+	int count = 2;
+
+	// Initialize candidate i.e. the number in which 
+	// we are testing the primality of.
+	unsigned long candidate = 5;
+
+	clock_t begin = clock();
 	do 
 	{
-
-		l_last += 1;
-
+		// Assume it is prime unless proven otherwise in
+		// the following for-loop.
 		bool isPrime = true;
-		
-		for (int i = 0; i < primes.size(); i++)
+
+		// We use trial division using all odd numbers from
+		// 3 to the smallest integer greater than or
+		// equal to the root of the candidate.
+
+		int div_bound = ceil(sqrt(candidate));
+		for (int i = 3; i <= div_bound; i+=2)
 		{
-			if (fmod(l_last, primes[i]) == 0)
+			if (candidate % i == 0)
 			{
 				isPrime = false;
 				break;
 			}
 		}
 
+		// If the candidate is prime
 		if (isPrime)
 		{
-			primes.push_back(l_last);
-			l_last = primes.back();
+
+			// Then we add it to the array,
+			// and increment the count.
+			primes[count] = candidate;
+			count++;
 		}
 
-	} while (primes.size() < bound);
+		// We check only odd numbers.
+		candidate += 2;
 
-	std::cout << primes.back();
+	} while (count < BOUND);
+
+	clock_t end = clock();
+	double secs = end - begin / CLOCKS_PER_SEC;
+	std::cout << secs << std::endl;
+	std::cout << primes[BOUND - 1];
 
 	return 0;
 
 }
+
+/* NOTES:
+
+ - At one point had the for-loop's conditional set to
+   " i < bound", but this excludes perfect squares.
+ - Arrays must have a declared size at compile-time, 
+   unless dynamically allocated (I assume).
+ - Had a previous version that did the same thing, but
+   used an std::vector. Much slower, presumably due to 
+   the index-out-of-range checking, as well as the resizing.
+ - This one helped me appreciate how expensive function calls
+   are as well. For example, calling ceil() and sqrt() 
+   within the for-loop's conditional multiple times 
+   makes the program run about 8x-9x slower.
+
+   */
